e9d4d06521e10cb1dc650e86a38acc7fff510141
Tsios Valentyn <valikyou75@gmail.com>
2024-12-30 22:34:05 +0200

    Додано клас Order та тест CalculateTotal_ShouldReturnCorrectTotal

--- Lab3/Order.cs
+++ Lab3/Order.cs
@@ -6,6 +6,22 @@ using System.Threading.Tasks;
 
 namespace Lab3
 {
-   
+    public class Order
+    {
+        public int OrderId { get; private set; } // Унікальний ідентифікатор замовлення
+        public List<Dish> OrderedDishes { get; private set; } // Список страв у замовленні
+        public string Status { get; private set; } // Статус замовлення ("Очікує", "Готується", "Готово")
+
+        // Конструктор замовлення
+        public Order(int orderId)
+        {
+            OrderId = orderId;
+            OrderedDishes = new List<Dish>();
+            Status = "Очікує";
+        }
+
+
+    }
+
 
 }
--- MenuAndOrderLibrary.Tests/OrderTests.cs
+++ MenuAndOrderLibrary.Tests/OrderTests.cs
@@ -9,7 +9,20 @@ using Xunit;
 
 namespace MenuAndOrderLibrary.Tests
 {
+    public class OrderTests
+    {
+        [Fact]
+        public void CalculateTotal_ShouldReturnCorrectTotal()
+        {
+            var order = new Order(1);
+            order.OrderedDishes.Add(new Dish("Суп", 50.5m));
+            order.OrderedDishes.Add(new Dish("Салат", 30.25m));
 
-}
+            var total = order.CalculateTotal();
+
+            Assert.Equal(80.75m, total);
+        }
+
+    }
 
    
