802345bc7d8b768624c09a01b95567f76b6071c1
Tsios Valentyn <valikyou75@gmail.com>
2024-12-30 22:38:10 +0200

    Додано методи CalculateTotal та UpdateOrderStatus

--- Lab3/Order.cs
+++ Lab3/Order.cs
@@ -19,7 +19,30 @@ namespace Lab3
             OrderedDishes = new List<Dish>();
             Status = "Очікує";
         }
+        // Метод для оновлення статусу замовлення
+        public void UpdateOrderStatus(string newStatus)
+        {
+            if ((Status == "Очікує" && newStatus == "Готується") ||
+                (Status == "Готується" && newStatus == "Готово"))
+            {
+                Status = newStatus;
+            }
+            else
+            {
+                throw new InvalidOperationException("Неправильна послідовність оновлення статусу.");
+            }
+        }
 
+        // Метод для обчислення загальної вартості замовлення
+        public decimal CalculateTotal()
+        {
+            decimal total = 0;
+            foreach (var dish in OrderedDishes)
+            {
+                total += dish.Price;
+            }
+            return total;
+        }
 
     }
 
